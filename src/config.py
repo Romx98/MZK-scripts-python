394f6c3767edac33efb8af8a251f067ad4b8d7aa
Romx98 <romanklampar@gmail.com>
2022-03-31 20:56:29 +0200

    Add config for database

---
+++ src/config.py
@@ -0,0 +1,20 @@
+import os.path
+from os.path import exists
+from yaml import safe_load
+
+
+def load_config(file_name="/../config.yaml"):
+    file = os.path.dirname(__file__) + file_name
+    if exists(file):
+        with open(file, "r") as f:
+            config = safe_load(f)
+        return config
+    return {}
+
+
+def db_config():
+    try:
+        return load_config()["DATABASE"]
+    except KeyError:
+        print("[!] Missing field in config file!")
+        return {}
